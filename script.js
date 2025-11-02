d275f467a079763e3513e6e0bbeb46c413702a1d
TheOwlLaw <79551992+TheOwlLaw@users.noreply.github.com>
2025-11-01 21:28:46 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+window.$crisp = [];
+window.CRISP_WEBSITE_ID = "6d225f4f-c773-479b-8ce3-4da209f5ed0a"; // 👈 Replace with your Crisp ID
+
+(function() {
+  const d = document;
+  const s = d.createElement("script");
+  s.src = "https://client.crisp.chat/l.js";
+  s.async = 1;
+  d.getElementsByTagName("head")[0].appendChild(s);
+})();
